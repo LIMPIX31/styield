c872e1b6da6f7483545585124ad9838cfad50f82
LIMPIX31 <limpix31@gmail.com>
2023-01-19 18:59:44 +0100

    feat: 2.0.0

--- README.md
+++ README.md
@@ -28,26 +28,129 @@ export const Button = styled.button(({ disabled, primary, danger }) => {
 
 ## Styield
 ```ts
-export const Button = styled.button<ButtonProps>(({
-    theme, disabled, variant
-  }) => build(function* () {
-  yield styield
-    .display('flex')
-    .alignItems('center')
-    .justifyContent('center')
-    .gap('6px')
-    .padding('10px 20px')
-    .ifNot(disabled, styield.boxShadow('0 0 15px 0 rgba(0, 0, 0, .1)'))
-    .switch(variant, {
-      'primary': styield.backgroundColor('blue'),
-      'destructive': styield.backgroundColor('blue'),
-      'link-gray': 'pass',
-      'link-color': styled.backgroundColor(variant.includes('color') ? 'gray' : 'pink')
-    })
-  if (disabled) yield styield.if(
-    destructive,
-    styield.color('#ff00007f'),
-    styield.color('#00ff007f')
-  )
-}))
+export const StyledButton = styled.button<ButtonProps>(
+  ({ theme, variant = 'default', destructive, only, size = 'md', disabled }) =>
+    build(
+      styield
+        .display('flex')
+        .alignItems('center')
+        .justifyContent('center')
+        .borderRadius(theme.radius())
+        .gap(theme.space())
+        .boxSizing('border-box')
+        .transition('all .2s')
+        .userSelect('none')
+        .borderWidth('2px')
+        .borderStyle('solid')
+        .ifNot(disabled, styield.cursor('pointer'))
+        .switch(size, {
+          sm: styield
+            .var('size', '30px')
+            .fontSize('0.825rem')
+            .padding(`0 ${theme.space(2)}`),
+          lg: styield
+            .var('size', '42px')
+            .fontSize('1.15rem')
+            .padding(`0 ${theme.space(4)}`),
+          default: styield
+            .var('size', '38px')
+            .fontSize('1rem')
+            .padding(`0 ${theme.space(3)}`),
+        })
+        .height.var('size')
+        .if(only, styield.width.var('size'))
+        .lineHeight('0px')
+        .switch(variant, {
+          primary() {
+            const color = destructive ? theme.palette.red : theme.accent.primary
+            const lighted = pick(color, 600)
+            return styield.color(theme.master.back).if(
+              disabled,
+              styield
+                .var('disabled-color', pick(theme.master.front, 100))
+                .background.var('disabled-color')
+                .borderColor.var('disabled-color'),
+              styield
+                .backgroundColor(color)
+                .borderColor(color)
+                .selector('&:hover', styield.backgroundColor(lighted).borderColor(lighted))
+                .selector('&:focus', styield.boxShadow(css`0 0 0 4px ${pick(color, 50)}`)),
+            )
+          },
+          secondary() {
+            const color = destructive ? theme.palette.red : theme.accent.primary
+            return styield.if(
+              disabled,
+              styield
+                .var('color', pick(color, 25))
+                .backgroundColor.var('color')
+                .borderColor.var('color')
+                .color(pick(color, 300)),
+              styield
+                .var('color', pick(color, 50))
+                .backgroundColor.var('color')
+                .borderColor.var('color')
+                .color(color)
+                .selector(
+                  '&:hover',
+                  styield
+                    .var('color', pick(color, 60))
+                    .backgroundColor.var('color')
+                    .borderColor.var('color')
+                    .color(pick(color, 600)),
+                )
+                .selector('&:focus', styield.boxShadow(css`0px 0px 0px 4px ${pick(color, 25)}`)),
+            )
+          },
+          'tertiary-gray': true,
+          'tertiary-color'() {
+            const color = destructive
+              ? theme.palette.red
+              : variant === 'tertiary-gray'
+                ? theme.palette.gray
+                : theme.accent.primary
+            return styield
+              .borderColor('transparent')
+              .if(
+                disabled,
+                styield.color(pick(color, 300)),
+                styield
+                  .color(pick(color, 500))
+                  .selector('&:hover', styield.color(pick(color, 600)).background(pick(color, 50))),
+              )
+          },
+          'link-gray': true,
+          'link-color'() {
+            const color = destructive
+              ? theme.palette.red
+              : variant === 'link-gray'
+                ? theme.palette.gray
+                : theme.accent.primary
+            return styield
+              .borderWidth('0px')
+              .padding('0px')
+              .height('auto')
+              .if(
+                disabled,
+                styield.color(pick(color, 300)),
+                styield.color(pick(color, 600)).selector('&:hover', styield.color(pick(color, 700))),
+              )
+          },
+          default() {
+            const color = destructive ? theme.palette.red : theme.palette.gray
+            return styield.if(
+              disabled,
+              styield.borderColor(pick(color, 50)).color(pick(color, 100)),
+              styield
+                .borderColor(pick(color, 300))
+                .color(pick(color, 700))
+                .var('color', pick(color, 50))
+                .selector('&:hover', styield.backgroundColor.var('color'))
+                .selector('&:focus', styield.boxShadow(`0 0 0 4px ${styield.var('color')}`)),
+            )
+          },
+        })
+        .if(variant.match(/^(primary|secondary|default)$/), styield.boxShadow('0px 1px 2px rgba(16, 24, 40, 0.05)')),
+    ),
+)
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,79 @@
 import { CSSProperties } from 'react'
-import kebabCase from 'kebab-case'
-import { css, css as sccss, FlattenInterpolation, FlattenSimpleInterpolation } from 'styled-components'
+import * as kebabCase from 'kebab-case'
+import { css as sccss, FlattenInterpolation, FlattenSimpleInterpolation } from 'styled-components'
 
-const meta = ['then', 'if', 'ifNot', 'switch', 'selector'] as const
+const meta = ['then', 'if', 'ifNot', 'switch', 'selector', 'var', 'use'] as const
 type MetaProperty = typeof meta[number]
 
-type CSSS = { [P in keyof CSSProperties]-?: (value: CSSProperties[P] | FlattenSimpleInterpolation | FlattenInterpolation<any>) => StyleBuilder }
+type CSSS = { [P in keyof CSSProperties]-?: ((value: CSSProperties[P] | FlattenSimpleInterpolation | FlattenInterpolation<any>, important?: boolean) => StyleBuilder) & { var: (name: string) => StyleBuilder } }
 
 interface Styield extends CSSS {
   _build(): FlattenSimpleInterpolation[]
-  if(condition: any, style: StyleBuilder): StyleBuilder
-  ifNot(condition: any, style: StyleBuilder): StyleBuilder
-  switch(target: any, matcher: Switch): StyleBuilder
-  selector(selector: string, style: StyleBuilder): StyleBuilder
+  if(condition: any, Then: StyieldGenerator, Else?: StyieldGenerator): StyleBuilder
+  ifNot(condition: any, Them: StyieldGenerator, Else?: StyieldGenerator): StyleBuilder
+  switch<T extends string | number>(target: T | undefined, matcher: Switch<T>): StyleBuilder
+  selector(selector: string, style: StyieldGenerator): StyleBuilder
+  var(name: string, value: any): StyleBuilder
+  var(name: string): string
+  use(style: StyieldGenerator): StyleBuilder
 }
 
-interface Switch {
-  [k: string | number | 'default']: StyleBuilder | 'pass'
+export type Switch<T extends string | number> = {
+  [P in T | 'default']?: StyieldGenerator | true
 }
 
 interface Builder {
   css: FlattenSimpleInterpolation[]
 }
 
-export type StyleBuilder = Styield & Omit<Builder, 'css'>
+export type StyleBuilder = Styield & Builder
 
-export type StyieldGenerator = Generator<StyleBuilder> | (() => Generator<StyleBuilder | StyieldGenerator>)
+export type StyieldGenerator = StyleBuilder | ((builder: StyleBuilder) => StyleBuilder)
+
+const resolve = (gen: StyieldGenerator) => typeof gen === 'function' ? gen(styield) : gen
 
 const createBuilder = (css: FlattenSimpleInterpolation[] = []) => new Proxy<Builder>({ css }, {
   get(builder: Builder, p: keyof CSSProperties & MetaProperty): any {
     if (p === 'css') return builder.css
     const next = (...csss: FlattenSimpleInterpolation[]) => createBuilder(builder.css.concat(csss))
     switch (p) {
-      case '_build': return () => builder.css
-      case 'if': return (condition: any, Then: Builder, Else: Builder) => !!condition ? next(...Then.css) : Else ? next(...Else.css) : createBuilder(builder.css)
-      case 'ifNot': return (condition: any, Then: Builder, Else: Builder) => !condition ? next(...Then.css) : Else ? next(...Else.css) : createBuilder(builder.css)
-      case 'selector': return (selector: string | FlattenSimpleInterpolation, value: Builder) =>
-        next([sccss`${selector} {\n`, ...value.css, sccss`}`])
-      case 'switch': return (target: any, matcher: Switch) => {
-        let pass = false
-        for (const kase in matcher) {
-          if (matcher[kase] === 'pass') pass = true
-          else if (target === kase || pass) return next(...(matcher[kase] as unknown as Builder).css)
-          else pass = false
+      case '_build':
+        return () => builder.css
+      case 'if':
+        return (condition: any, Then: StyieldGenerator, Else: StyieldGenerator) =>
+          !!condition ? next(...resolve(Then).css) : Else ? next(...resolve(Else).css) : createBuilder(builder.css)
+      case 'ifNot':
+        return (condition: any, Then: StyieldGenerator, Else: StyieldGenerator) =>
+          !condition ? next(...resolve(Then).css) : Else ? next(...resolve(Else).css) : createBuilder(builder.css)
+      case 'selector':
+        return (selector: string | FlattenSimpleInterpolation, value: StyieldGenerator) =>
+          next([sccss`${selector} {\n`, ...resolve(value).css, sccss`}`])
+      case 'var':
+        return (name: string, value?: string) => value ? next(sccss`--${name}: ${value};\n`) : `var(--${name})`
+      case 'use':
+        return (style: StyieldGenerator) => next(...resolve(style).css)
+      case 'switch':
+        return (target: any, matcher: any) => {
+          let pass = false
+          for (const kase in matcher) {
+            if (target === kase && matcher[kase] === true) pass = true
+            else if ((target === kase || pass) && matcher[kase] !== true) return next(...(resolve(matcher[kase]) as unknown as Builder).css)
+            else pass = false
+          }
+          return matcher.default && matcher.default !== 'pass' ? next(...(resolve(matcher.default) as unknown as Builder).css) : createBuilder(builder.css)
         }
-        return matcher.default && matcher.default !== 'pass' ? next(...(matcher.default as unknown as Builder).css) : createBuilder(builder.css)
-      }
     }
-    return (value?: any) => value ? next(sccss`${kebabCase(p)}: ${value};\n`) : createBuilder(builder.css)
-  }
+    const factory = (value?: any, important?: boolean) => value ? next(sccss`${kebabCase.default(p)}: ${value}${important ? ' !important' : ''};\n`) : createBuilder(builder.css)
+    Reflect.defineProperty(factory, 'var', { value: (name: string) => factory(`var(--${name})`) })
+    return factory
+  },
 }) as unknown as StyleBuilder
 
 const isBuilder = (value: any): value is StyleBuilder => 'css' in value
 
-export function build(generator: StyieldGenerator | StyleBuilder) : FlattenSimpleInterpolation {
-  if (isBuilder(generator)) return css({}, ...generator._build())
-  const gen = typeof generator === 'function' ? generator() : generator
-  const interpolations: FlattenSimpleInterpolation[] = []
-  for(const i of gen) interpolations.push(typeof i === 'function' ? build(i) : isBuilder(i) ? i._build() : build(i))
-  return css({}, ...interpolations)
+export function build(generator: StyieldGenerator): FlattenSimpleInterpolation {
+  const gen = typeof generator === 'function' ? generator(styield) : generator
+  return sccss({}, ...gen._build())
 }
 
 export const styield = createBuilder()
